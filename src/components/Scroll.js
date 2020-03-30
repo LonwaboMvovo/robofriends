d73063139a137e8b49efd0ded309ae4a8796ea88
Lonwabo M <44nator@gmail.com>
2020-03-30 22:24:59 +0200

    deploying the app

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -2,11 +2,7 @@ import React from 'react';
 
 const Scroll = (props) => {
     return (
-<<<<<<< HEAD
         <div style={{ overflowY: 'scroll', border: '1px solid black', height: 'calc(100vh - 157.24px)' }}>
-=======
-        <div style={{ overflowY: 'scroll', border: '1px solid black', height: '500px' }}>
->>>>>>> d005d649b6e578917c99a9827c9ff93971f457dc
             {props.children}
         </div>
     )
