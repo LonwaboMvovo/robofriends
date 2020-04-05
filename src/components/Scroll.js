3e31f04aa8136a4d2107e002bd5b2e3313d86830
Lonwabo Mvovo <44nator@gmail.com>
2020-04-05 16:08:02 +0200

    changed overflow to only overflow-y

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -4,10 +4,10 @@ import './Scroll.css';
 const Scroll = (props) => {
   return (
     <div>
-      <div className='desktop' style={{ overflow: 'scroll', height: 'calc(100vh - 187.32px)'}}>
+      <div className='desktop' style={{ overflowY: 'scroll', height: 'calc(100vh - 187.32px)'}}>
         {props.children}
       </div>
-      <div className='mobile' style={{ overflow: 'scroll', height: 'calc(100vh - 157.24px)'}}>
+      <div className='mobile' style={{ overflowY: 'scroll', height: 'calc(100vh - 157.24px)'}}>
       {props.children}
       </div>
     </div>
